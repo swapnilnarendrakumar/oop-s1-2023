39276534e7eb5c113cea86e71bd1d663cf217279
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-03 22:04:38 +1030

    Add files via upload

---
+++ function-3-4.cpp
@@ -0,0 +1,37 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+void print_pass_fail(char grade){
+    switch (grade){
+        case 'A':
+
+            cout << "Pass";
+            break;
+
+        case 'B':
+            cout << "Pass";
+            break;
+
+        case 'C':
+            cout << "Pass";
+            break;
+
+        case 'D':
+            cout << "Fail";
+            break;
+
+        case 'F':
+             cout << "Fail";
+             break;
+
+    }
+
+
+}
+
+
+
+
