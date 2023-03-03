48c9c43149ba42339cb6e5dcf0365a115d3b6c43
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-03 21:53:36 +1030

    Add files via upload

---
+++ function-2-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+// The `extern` keyword tells the compiler that the count function exists,
+// but will be implemented somewhere else
+
+bool is_descending(int array[], int n){
+    int total = 0;
+    for (int i = 0; i < n - 1; i++){
+		if (array[i] < array[i + 1]) {
+			total =  0;
+		} else {
+        total = 1;
+    }
+	} 
+    return total ;
+}
+
+
+
