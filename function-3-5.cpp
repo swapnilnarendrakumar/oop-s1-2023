5ea646b0de7ef9e06c1159c13aa24df329aaaea1
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-05 10:00:50 +1030

    Add files via upload

---
+++ function-3-5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+
+int sum_even(double array[], int n){
+    int total = 0;
+ 
+
+    for(int i=0;i<n;i=i+2){ 
+           total += (array[i]);
+    }
+
+    return total;
+}
+
+
+int main() {
+    double array[5] = {4,5,6,7,8};
+    std::cout << "The number is: " << sum_even (array, 5) << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
