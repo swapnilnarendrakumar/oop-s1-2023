424fb3e9269f0e6a83894a0662a92c4710310894
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-05 09:30:47 +1030

    Add files via upload

---
+++ function-2-3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// The `extern` keyword tells the compiler that the count function exists,
+// but will be implemented somewhere else
+extern  int two_five_nine(int array[], int n);
+int two_five_nine(int array[], int n){
+    int total = 0;
+    int total2 = 0;
+    int total3 = 0;
+    for (int i = 0; i < n; i++) {
+        if (array[i] == 2) {
+            total += 1;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (array[i] == 5) {
+            total2 += 1;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (array[i] == 9) {
+            total3 += 1;
+        }
+    }
+
+    cout << "2: " << total << "5: " << total2  << "9: " << total3 << endl;
+
+    return total;
+}
+
+
+
