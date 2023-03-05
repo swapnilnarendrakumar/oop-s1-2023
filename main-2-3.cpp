80adc16d0638bfe695ec30634bbd1ac766de0565
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-05 10:54:22 +1030

    Update main-2-3.cpp

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -9,5 +9,5 @@ extern int two_five_nine(int array[], int n);
 int main() {
     int array[5] = {4,5,6,7,8};
     cout << "The number is: " << two_five_nine(array, 5) << endl;
-    return 0;
+    
 }
