cebb8c975064197af5e2c1859512c17f4b41b9ab
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-08 14:51:34 +1030

    Add files via upload

---
+++ workshop2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+int main() {
+    int boy = 75;
+
+    int boy2 = 10011001;
+    int x1;
+
+  
+
+    while (boy2 / 2 != 0){
+        x1 = boy % 10;
+        boy = boy / 10;
+        //cout << boy << endl;
+        //x = boy % 2;
+        
+        cout << x1;
+        
+    }
+
+    
+
+
+   
+}
