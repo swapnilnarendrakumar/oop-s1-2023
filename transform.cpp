32424b0d886b698ac6bba88543c98a49e598fc6a
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-08 14:54:48 +1030

    Add files via upload

---
+++ transform.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+int main() {
+    int boy = 75;
+    int x;
+    int boy2 = 10011001;
+    int x1;
+
+    while (boy / 2 != 0){
+        x = boy % 2;
+        boy = boy/2;
+        //cout << boy << endl;
+        //x = boy % 2;
+        
+        cout << x;
+        
+    }
+
+    /*while (boy2 / 2 != 0){
+        x1 = boy % 10;
+        boy = boy / 10;
+        //cout << boy << endl;
+        //x = boy % 2;
+        
+        cout << x1;
+        
+    }*/
+
+    
+
+
+   
+}
+
+   
