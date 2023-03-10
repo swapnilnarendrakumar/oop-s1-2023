fe12ad66cbc0de7a8ab4a5070b3075877c367c3c
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-10 22:06:47 +1030

    Add files via upload

---
+++ function-1-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+int is_identity(int array[10][10]){
+    int total=0;
+     for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            
+            if ( array[i][j] != 1 && array[j][i] != 0){
+
+                
+                total = 0;
+                break;
+
+            }
+            if ( i == j && array[j][i] != 0){
+                
+                total = 0;
+                break;
+
+            }
+            /*if ( i != j && array[j][i] != 0){
+                
+                total = 0;
+                break;
+
+            }*/
+            else{
+                total = 1;
+            }
+        }
+        
+    }
+  
+    return total;
+}
