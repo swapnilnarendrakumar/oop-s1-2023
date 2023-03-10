1f4a6d0c6afb5575cd26305d69d47615bebcc2a3
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-10 14:17:32 +1030

    Add files via upload

---
+++ main-2-3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+// The `extern` keyword tells the compiler that the count function exists,
+
+bool is_palindrome(int integers[], int length){
+
+    int total = 0;
+    for (int i = 0; i <= length / 2; i++) {
+        if (integers[i] != integers[length - i - 1]) {
+            total = 0;
+            break;
+        }
+        if (integers[i] == integers[length - i - 1]) {
+            total = 1;
+        }
+
+
+    }
+
+    return total;
+}
+
+ int sum_array_elements(int integers[], int length){
+    int sum;
+    int add;
+
+    for (int i = 0; i < length; i++) {
+            sum += integers[i];
+        
+    }
+
+    return sum;
+}
+
+int sum_if_palindrome(int integers[], int length){
+    int sum;
+    bool result = is_palindrome(integers, length);
+    if (result == true){
+        sum = sum_array_elements(integers, length);
+    }
+    return sum;
+}
+
+
+
+
+int main() {
+    int array[5] = {4,5,6,7,8};
+    int array2[5] = {1,1,0,1,1};
+    int array3[5] = {1,1,0,1,3};
+    int array4[5] = {1,1,0,2,1};
+    int array5[5] = {3,1,0,1,3};
+    int integers[3] = {1,2,1};
+    cout << "The number is: "<<boolalpha <<is_palindrome(array, 5) << endl;
+    cout << "The number is: "<<boolalpha <<is_palindrome(array2, 5) << endl;
+    cout << "The number is: "<<boolalpha <<is_palindrome(array3, 5) << endl;
+    cout << "The number is: "<<boolalpha <<is_palindrome(array4, 5) << endl;
+    cout << "The number is: "<<boolalpha <<is_palindrome(array5, 5) << endl;
+    cout << "The number is: " <<sum_if_palindrome(array5, 5) << endl;
+    return 0;
+}
