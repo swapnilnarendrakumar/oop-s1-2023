1823c823332f2cc3310a3b23118631031ca53d80
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-15 12:21:26 +1030

    Add files via upload

---
+++ function-1-4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int size_of_array_arr(){
+    int *arr = new int[3];
+
+    return sizeof(arr);
+}
+
--- main-1-5.cpp
+++ main-1-5.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 int main(){
-    int *array = new int[7];
+    cout<< hello;
     //int array[7] = {1,2,3,4,5,6,7};
 
     //int x = sizeof(array);
