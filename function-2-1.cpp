0564a16081703f7a565c42d0cb0f8594a86c81ff
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-15 13:15:53 +1030

    Add files via upload

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 float add_op(float left, float right){
-    float x, y, sum;
+    float sum;
     float *ptr1, *ptr2;
-    ptr1 = &x; //assigning an address to pointer
-    ptr2 = &y;
+    ptr1 = &left; //assigning an address to pointer
+    ptr2 = &right;
     sum = *ptr1 + *ptr2;
     return sum;
 
 }
 float subtract_op(float left, float right){
-    float x, y, subtraction;
+    float subtraction;
     float *ptr1, *ptr2;
-    ptr1 = &x; //assigning an address to pointer
-    ptr2 = &y;
+    ptr1 = &left; //assigning an address to pointer
+    ptr2 = &right;
     subtraction = *ptr1 - *ptr2;
     return subtraction;
 
