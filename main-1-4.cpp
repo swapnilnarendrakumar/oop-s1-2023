b3714bf26e6b919f5f2f1b4be61dd86e87653a63
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-15 12:27:52 +1030

    Add files via upload

---
+++ main-1-4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+extern size_of_variable_star_arr();
+
+int main(){
+    cout << "hellol";
+}
---
+++ main-1-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+extern int size_of_array_arr();
+
+int main(){
+    cout<< hello;
+    //int array[7] = {1,2,3,4,5,6,7};
+
+    //int x = sizeof(array);
+    //int y = sizeof(array)/sizeof(array[0]);
+    //cout << x << endl;
+    //cout << y;
+}
