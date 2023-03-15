d26f43d79a025cc09252a813fa9654be00bc3499
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-15 13:50:04 +1030

    Add files via upload

---
+++ function-2-4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+float add_op(float left, float right){
+    float sum;
+    sum = left + right;
+    return sum;
+
+}
+float subtract_op(float left, float right){
+    float subtraction;
+    subtraction = left - right;
+    return subtraction;
+
+}
+
+float arithmetic_ops(float left, float right, std::string op){
+        
+
+    if (op == "+"){
+         cout <<  left + right;
+    }
+    if (op == "-"){
+         cout << left - right;
+    }
+
+
+   
+}
+
+float multiply_op(float left, float right){
+    float product;
+    subtraction = left * right;
+    return product;
+
+}
