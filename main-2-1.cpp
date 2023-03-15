2db1557eda460288e5983fecad48816b006567ec
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-15 14:02:06 +1030

    Add files via upload

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 extern float add_op(float left, float right);
 extern float subtract_op(float left, float right);
-float arithmetic_ops(float left, float right, std::string op);
+extern float arithmetic_ops(float left, float right, std::string op);
 
 int main(){
+
+    cout << arithmetic_ops(1.8, 2.9, "add_op");
+
 
 
 }
