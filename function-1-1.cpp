c06e4df27a2f3e974f4c1563b6caa69eee5a87e5
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-16 16:16:40 +1030

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void print_matrix(int array[10][10]){
     for (int i = 0; i < 10; i++){
         for (int j = 0; j < 10; j++){
-            cout<< array[i][j] << "";
+            cout<< array[i][j] << " ";
         }
         cout << endl;
 
