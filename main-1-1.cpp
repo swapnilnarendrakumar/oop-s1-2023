481d6ff5aef84d56f3206ed2f775f5c902b673d3
swapnilnarendrakumar <113482728+swapnilnarendrakumar@users.noreply.github.com>
2023-03-30 16:35:11 +1030

    Add files via upload

---
+++ main-1-1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include "truck.h"
+
+
+
+
+int main(){
+    truck truck1;
+    truck1.get_b_code();
+    truck1.get_b_name();
+    cout<< "brand name:"<< truck1.b_name << endl;
+    cout << "brand code:"<< truck1.b_code<< endl;
+
+
+    return 0;
+
+
+
+    
+}
+
